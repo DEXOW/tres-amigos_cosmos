6a83c17c3b7f5c45c57a353428d9c09f65dd7208
Thinal Perera <thinaltp@gmail.com>
2023-08-19 23:53:46 +0530

    Adding providers, register and login screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,48 @@
+import 'dart:math';
+
+import 'package:cosmos/screens/components/animated_star_background.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
-import 'package:cosmos/screens/splash/screen.dart';
 import 'package:cosmos/constants.dart';
+import 'package:cosmos/screens/splash/screen.dart';
+import 'package:cosmos/providers/meta_data_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: MetaDataProvider()),
+      ],
+      child: MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  late MetaDataProvider metaDataProvider;
+  final Random random = Random();
+
+  void initStars(screenWidth, screenHeight,context){
+    metaDataProvider = Provider.of<MetaDataProvider>(context);
+    metaDataProvider.stars = List.generate(30, (index) {
+      return Star(
+        x: random.nextDouble() * screenWidth,
+        y: random.nextDouble() * screenHeight,
+        size: random.nextDouble() * 5 + 1,
+        opacity: random.nextDouble(),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    
+    initStars(MediaQuery.of(context).size.width, MediaQuery.of(context).size.height, context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Cosmos',
@@ -24,7 +54,7 @@ class MyApp extends StatelessWidget {
       ),
       home: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light.copyWith(systemNavigationBarColor: kPrimaryColor),
-        child: GetStartedScreen(),
+        child: SplashScreen(),
       ),
     );
   }
